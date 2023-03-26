9e3c52b8151f22b5a9526c417ba4037f4221cf92
Sebastian Castellanos Sandoval <u1201755@unimilitar.edu.co>
2023-03-25 19:57:59 -0500

    Se ajusta la meta descripcion y se añaden dos .json de traduccion para paginal de esal y tratamiento de datos

--- src/routes/Politicadedatos.jsx
+++ src/routes/Politicadedatos.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
+import { useTranslation } from 'react-i18next'
+
 export const Politicadedatos = () => {
+    const [ t, i18n ] = useTranslation("datos");
+
     return (
         <>
             <header className='header-bg-publicos header-plantilla mb-5'>
                 <div className='max-w-[1200px] mx-auto'>
                     <div className='empatados-descripcion'>
-                        <h1 className='uppercase text-4xl font-bold text-white mb-4 md:text-6xl lg:text-7xl'>Tratamiento y uso de datos</h1>
+                        <h1 className='uppercase text-4xl font-bold text-white mb-4 md:text-6xl lg:text-7xl'>{t("header.header-title")}</h1>
                     </div>
                 </div>
             </header>
